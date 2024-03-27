8e3ebfc7cca3de28621920a2caf52efd7524e2ba
TharunRaj-03 <tharunraj.20ec@kct.ac.in>
2024-03-27 23:32:45 +0530

    req and packages 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 
 
-model = tf.keras.models.load_model(r'model_1.h5')
+model = tf.keras.models.load_model(r'model_1_h5.h5')
 
 
 st.title('Deepfakes Classification - Images')
